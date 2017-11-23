478a281511bbe66e8e5a32751a8cdb3d36ed8947
FelipeVeras0001 <33816819+FelipeVeras0001@users.noreply.github.com>
2017-11-23 15:13:41 -0200

    Add files via upload

---
+++ pratica5/pratica5.sql
@@ -0,0 +1,27 @@
+-- Criando banco
+CREATE DATABASE pratica5;
+
+USE pratica5;
+
+
+-- Criando tabela para multimidia
+create table dados_multimidia (
+codigo int auto_increment not null,
+nome varchar(30), 
+tipo varchar (20),
+dados longblob,
+primary key (codigo)
+);
+
+-- Executando variavel
+show variables like "secure_file_priv";
+
+-- Inserindo dados_multimidia
+insert into dados_multimidia (nome, tipo, dados) values ( 'imagem1', 'jpg', load_file("C:\\ProgramData\\MySQL\\MySQL Server 5.7\\Uploads\\imagem1.jpg"));
+insert into dados_multimidia (nome, tipo, dados) values ( 'imagem2', 'jpg', load_file("C:\\ProgramData\\MySQL\\MySQL Server 5.7\\Uploads\\imagem2.jpg"));
+
+select * from dados_multimidia;
+
+select dados into outfile 'C:\\ProgramData\\MySQL\\MySQL Server 5.7\\Uploads\\gio.jpeg' 
+from dados_multimidia 
+where codigo = 2;
